010fada129957e2a3ab28de85993d33b728cebc5
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2020-10-26 12:48:44 -0300

    onscroll

--- javascript/js.js
+++ javascript/js.js
@@ -1,9 +1,41 @@
-function suporte() {
+function scrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
 
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
 
-    var suporte = document.getElementById('supo');
-    suporte.style.backgroundColor = 'red';
+        console.log(section);
+    }
 
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
 
-};
+}
+
+scrollSuave();
+
+function onScrollMenu (){
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    const menu = document.getElementById("menu js-menu");
+    if (document.documentElement.scrollTop > menu.offsetTop) {
+        menu.style.position = "fixed";
+        menu.style.top = "0";
+    } else {
+        menu.style.position = "relative"
+
+    }
+}
+}
+onScrollMenu();
+
